1487e5f12db0df36e44dbe39aad6634a368de863
Filip Tammergård <filip@tammergard.se>
2021-08-20 20:29:23 +0200

    feat: limit header width

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,4 @@
-<header>
+<header class="max-w-6xl mx-auto">
 	<nav class="p-4 w-full text-center sm:flex justify-around">
     <a class="block" href="/">Namedays</a>
     <a class="block" href="/saved-namedays">Saved Namedays</a>
